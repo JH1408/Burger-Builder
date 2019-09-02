b0c88e84211afd6e70d2efc2c52e70260ba2766e
Josy Hartig <josy_hartig@yahoo.de>
2019-09-02 19:15:36 +0800

    restructuring

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,10 +3,8 @@ import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    if(nextProps.show !== this.props.show || nextProps.children !== this.props.children) {
-      return true
-    }
+  shouldComponentUpdate ( nextProps, nextState ) {
+      return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
 
   render() {
