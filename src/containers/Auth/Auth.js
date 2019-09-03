7779e52076c3c14d9ca6b25c48ddf2c38b0644f6
Josy Hartig <josy_hartig@yahoo.de>
2019-09-03 16:21:16 +0800

    add authentication

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,158 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import classes from './Auth.module.css';
+import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
+
+class Auth extends Component {
+  state = {
+    controls: {
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'E-Mail'
+        },
+        value: '',
+        valueType: 'E-Mail',
+        validation: {
+          required: true,
+          isEmail: true
+        },
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Password'
+        },
+        value: '',
+        valueType: 'Password',
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
+      },
+    },
+      isSignUp: true
+  }
+
+  checkValidity (value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(value) && isValid
+    }
+    if (rules.isNumeric) {
+        const pattern = /^\d+$/;
+        isValid = pattern.test(value) && isValid
+    }
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+        touched: true
+      }
+    };
+    this.setState({controls: updatedControls});
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp )
+  }
+
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return{isSignUp: !prevState.isSignUp};
+    });
+  }
+
+  render () {
+    const formElementsArray = [];
+    for (let key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key]
+      })
+    }
+
+    let form = formElementsArray.map(formElement => (
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        valueType={formElement.config.valueType}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={(event) => this.inputChangedHandler (event, formElement.id)}/>
+    ));
+
+    if (this.props.loading) {
+      form = <Spinner />
+    }
+
+    String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  }
+    let errorMessage = null;
+    if (this.props.error) {
+      const error = (this.props.error.message).toLowerCase().capitalize().replace(/[_-]/g, " "); 
+      errorMessage = (
+        <p>{error}</p>
+      )
+    }
+
+    return (
+      <div className={classes.Auth}>
+        <h1>{this.state.isSignUp ? 'Sign Up' : 'Sign In'}</h1>
+        {errorMessage}
+        <form onSubmit={this.submitHandler}>
+          {form}
+          <Button btnType="Success">Submit</Button>
+        </form>
+          <Button
+            clicked={this.switchAuthModeHandler}
+            btnType="Danger">{this.state.isSignUp ? 'Already have an account? Sign in' : "Don't have an account? Sign up"}</Button>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
